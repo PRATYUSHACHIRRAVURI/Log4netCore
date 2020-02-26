1d857fa65199af1bb228f3fa28affa9107479cb4
Pratyusha Chirravuri <Pratyusha_Chirravuri@epam.com>
2020-02-26 11:15:49 +0530

    Add project files.

---
+++ Log4netCore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Log4netCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+      public ViewResult Details()
+        {
+            throw new Exception("Error in details action");
+        }
+    }
+}
